0b13a0f2cbafadde238618f7a71628e6d453ce19
Scuggle <ivoseetge@googlemail.com>
2022-12-01 21:55:26 +0100

    Added Changelog, only 8 GPs now

--- common/defines/cot_defines.lua
+++ common/defines/cot_defines.lua
@@ -1,6 +1,5 @@
 --Made to not mingle with the main defines.lua file
 
-NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 15
 NDefines.NMilitary.SLACKEN_AP_DROP = 0.025
 NDefines.NMilitary.SLACKEN_MANPOWER_INCREASE = 0.5
 NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.35
