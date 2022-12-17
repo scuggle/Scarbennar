d8416be8fabccd7a5d9590d01eebfa67248e50f4
scuggle <85515422+scuggle@users.noreply.github.com>
2022-12-17 16:10:14 +0100

    Revert "G UI development"

--- common/defines/00_UI.lua
+++
@@ -1,4 +0,0 @@
------------------------------ UI NDefines ----------------------------- 
-
-NDefines.NDiplomacy.MAX_CLIENT_STATES = 50  -- 最大仆从国数目
-NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 12  -- 最大列强数目
